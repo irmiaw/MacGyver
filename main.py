cfadcead80d82d88a69975fd6996380c56280dc7
irmiaw <j.c.comg@gmail.com>
2017-09-08 14:54:50 +0200

    Resumé des methodes basiques de pygame

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # bin/env python3
-import pygame
+import pygame  # ne pas tout importer !!
+# display, mixer, draw, event, image, mouse, time
 
+# # import de me propres modules
 # import game
 # import draw
 # import load
+
+pygame.init()  # init de tous les modules
+fenetre = pygame.display.set_mode((640, 480)) # création d'une fenêtre
+fond = pygame.image.load("background.jpg").convert() # charger une image
+perso = pygame.image.load("perso.png").convert_alpha() # charger une image avec fond transparent
+image.set_colorkey((255,255,255)) # rendre le blanc (valeur RGB : 255,255,255) de l'image transparent
+fenetre.blit(fond, (0,0)) # coller une image sur la fenêtre
+pygame.display.flip() # mettre à jour la fenêtre
+
+
+# Boucle principale
+continuer = 1
+while continuer:
+    continuer = int(input())
 
 # if __name__ == "__main__":
     # do stuff
