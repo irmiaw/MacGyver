aca1eb74bb94b8e70575076906f27c8bd430b187
irmiaw <j.c.comg@gmail.com>
2017-09-08 15:18:27 +0200

    Affichage d'une brique v1.0

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import pygame  # ne pas tout importer !!
 
 pygame.init()  # init de tous les modules
 fenetre = pygame.display.set_mode((640, 480)) # création d'une fenêtre
-fond = pygame.image.load("background.jpg").convert() # charger une image
-perso = pygame.image.load("perso.png").convert_alpha() # charger une image avec fond transparent
-image.set_colorkey((255,255,255)) # rendre le blanc (valeur RGB : 255,255,255) de l'image transparent
-fenetre.blit(fond, (0,0)) # coller une image sur la fenêtre
+#fond = pygame.image.load("background.jpg").convert() # charger une image
+brique = pygame.image.load("brique.png").convert_alpha() # charger une image avec fond transparent
+#image.set_colorkey((255,255,255)) # rendre le blanc (valeur RGB : 255,255,255) de l'image transparent
+fenetre.blit(brique, (320,240)) # coller une image sur la fenêtre
 pygame.display.flip() # mettre à jour la fenêtre
 
 
