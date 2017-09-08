aa1fae7c495d97e12f40fe445d59c580c9aa9a60
irmiaw <j.c.comg@gmail.com>
2017-09-08 16:13:20 +0200

    Affichage d'un mur de briques

--- main.py
+++ main.py
@@ -8,11 +8,45 @@ import pygame  # ne pas tout importer !!
 # import load
 
 pygame.init()  # init de tous les modules
-fenetre = pygame.display.set_mode((640, 480)) # création d'une fenêtre
+window = pygame.display.set_mode((640, 480)) # création d'une fenêtre
 #fond = pygame.image.load("background.jpg").convert() # charger une image
-brique = pygame.image.load("brique.png").convert_alpha() # charger une image avec fond transparent
+brick = pygame.image.load("brick.png").convert_alpha() # charger une image avec fond transparent
+floor = pygame.image.load("floor.png").convert_alpha()
 #image.set_colorkey((255,255,255)) # rendre le blanc (valeur RGB : 255,255,255) de l'image transparent
-fenetre.blit(brique, (320,240)) # coller une image sur la fenêtre
+#fenetre.blit(brick, (320,240)) # coller une image sur la fenêtre
+
+map = [["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+       [" ", " ", " ", " ", " ", " ", "#", " ", " ", "#", " ", " ", " ", " ", " "],
+       ["#", " ", " ", "#", "#", " ", "#", " ", "#", "#", " ", "#", " ", " ", "#"],
+       ["#", " ", "#", " ", " ", "#", "#", " ", " ", " ", " ", "#", "#", "#", "#"],
+       ["#", " ", " ", " ", " ", "#", " ", "#", " ", "#", "#", "#", " ", "#", "#"],
+       ["#", " ", " ", "#", " ", "#", " ", "#", " ", " ", " ", "#", " ", " ", " "],
+       ["#", " ", "#", "#", " ", "#", " ", " ", "#", "#", " ", " ", " ", "#", " "],
+       ["#", " ", "#", "#", " ", " ", "#", " ", " ", "#", "#", "#", " ", "#", " "],
+       ["#", " ", " ", " ", "#", " ", " ", "#", " ", " ", " ", " ", "#", " ", " "],
+       ["#", "#", "#", " ", " ", "#", " ", "#", "#", "#", " ", "#", "#", " ", "#"],
+       ["#", " ", " ", "#", " ", "#", " ", "#", " ", "#", " ", " ", " ", " ", "#"],
+       ["#", "#", " ", "#", " ", "#", "#", " ", " ", "#", "#", "#", " ", "#", "#"],
+       ["#", " ", " ", " ", " ", " ", " ", " ", "#", "#", " ", " ", " ", "#", "#"],
+       ["#", " ", "#", " ", "#", " ", "#", " ", " ", " ", " ", "#", " ", " ", " "],
+       ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+]
+
+tile_size = 20
+
+num_line = 0
+for line in map:
+    num_column = 0
+    for case in line:
+        x = num_column * tile_size
+        y = num_line * tile_size
+        if case == "#":
+            window.blit(brick, (x,y))
+        elif case == " ":
+            window.blit(floor, (x,y))
+        num_column += 1
+    num_line += 1
+
 pygame.display.flip() # mettre à jour la fenêtre
 
 
