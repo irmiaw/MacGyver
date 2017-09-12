3a651337b9ae426de5c6eeaa9bd3d6f0cc1b31c7
irmiaw <j.c.comg@gmail.com>
2017-09-12 15:24:34 +0200

    Gestion des deplacements de Mac Gyver et des collisions avec les murs et les bords de map (v1.6)

--- main.py
+++ main.py
@@ -1,87 +1,136 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-import pygame  # ne pas tout importer mais uniquement les modules necessaires
+import pygame  # import only necessary modules !
 # display, mixer, draw, event, image, mouse, time
-from pygame.locals import * # import de tous les constantes
+from pygame.locals import * # import all pygame constants
 
 
-# # import de me propres modules
+# # import my own modules
 # import game
-# import draw
+# import display
 # import load
 
-tile_size = 40
-
-class MacGyver:
-    x=0
-    y=1 * tile_size
-
-
-
-pygame.init()  # init de tous les modules
-window = pygame.display.set_mode((15 * tile_size, 15 * tile_size)) # création d'une fenêtre
-#fond = pygame.image.load("background.jpg").convert() # charger une image
-brick = pygame.image.load("brick.png").convert_alpha() # charger une image avec fond transparent
-floor = pygame.image.load("floor.png").convert_alpha()
-mac_gyver = pygame.image.load("mac_gyver.png").convert_alpha()
-guardian = pygame.image.load("murdoc.png").convert_alpha()
-brick = pygame.transform.scale2x(brick)
-floor = pygame.transform.scale2x(floor)
-#image.set_colorkey((255,255,255)) # rendre le blanc (valeur RGB : 255,255,255) de l'image transparent
-#fenetre.blit(brick, (320,240)) # coller une image sur la fenêtre
+# Constants
+TILE_SIZE = 40
 
 # '#': brick
 # ' ': floor
 # '.': guardian
-
-map = ["###############",
-       "      #  #     ",
-       "#  ## # ## #  #",
-       "# #  ##    ####",
-       "#    # # ### ##",
-       "#  # # #   #   ",
-       "# ## #  ##   # ",
-       "# ##  #  ### # ",
-       "#   #  #    #  ",
-       "###  # ### ## #",
-       "#  # # # #    #",
-       "## # ##  ### ##",
-       "#       ##   ##",
-       "# # # #    #  .",
-       "###############",
+maze_map = ["###############",
+            "      #  #     ",
+            "#  ## # ## #  #",
+            "# #  ##    ####",
+            "#    # # ### ##",
+            "#  # # #   #   ",
+            "# ## #  ##   # ",
+            "# ##  #  ### # ",
+            "#   #  #    #  ",
+            "###  # ### ## #",
+            "#  # # # #    #",
+            "## # ##  ### ##",
+            "#       ##   ##",
+            "# # # #    #  .",
+            "###############",
 ]
 
+class Character():
+    def __init__(self, maze_map):
+        self.case_position = {'x': 2, 'y': 1}
+        self.maze_map = maze_map
+        self.num_items = 0
 
-num_line = 0
-for line in map:
-    num_column = 0
-    for case in line:
-        x = num_column * tile_size
-        y = num_line * tile_size
+    def collect_items(self):
+        x = self.case_position['x']
+        y = self.case_position['y']
+        if self.maze_map[y][x] == '$':
+            self.maze_map[y][x] = ' '
+            self.num_items += 1
 
-        if case == "#":
-            window.blit(brick, (x,y))
-        elif case == " " or case == ".":
-            window.blit(floor, (x,y))
+    def go(self, direction):
+        x = self.case_position['x']
+        y = self.case_position['y']
+        if direction == "left":
+            if x > 0 and self.maze_map[y][x-1] != "#":
+                self.case_position['x'] -= 1
+        elif direction == "right":
+            if x < 14 and maze_map[y][x+1] != "#":
+                self.case_position['x'] += 1
+        elif direction == "up":
+            if y > 0 and maze_map[y-1][x] != "#":
+                self.case_position['y'] -= 1
+        elif direction == "down":
+            if y < 14 and maze_map[y+1][x] != "#":
+                self.case_position['y'] += 1
+        else:
+            print("error: unknown direction")
+        self.collect_items()
 
-        if case == ".":
-            window.blit(guardian, (x,y))
-        num_column += 1
-    num_line += 1
-
-window.blit(mac_gyver, (MacGyver.x ,MacGyver.y))
+    @property
+    def pixel_position(self):
+        return [self.case_position['x'] * TILE_SIZE, self.case_position['y'] * TILE_SIZE]
 
 
-pygame.display.flip() # mettre à jour la fenêtre
 
 
-continuer = 1
+def display(maze_map):
+    num_line = 0
+    for line in maze_map:
+        num_column = 0
+        for case in line:
+            x = num_column * TILE_SIZE
+            y = num_line * TILE_SIZE
 
+            if case == "#":
+                window.blit(image_brick, (x,y))
+            elif case == " " or case == ".":
+                window.blit(image_floor, (x,y))
+
+            if case == ".":
+                window.blit(image_guardian, (x,y))
+            num_column += 1
+        num_line += 1
+
+    window.blit(image_mac_gyver, mac_gyver.pixel_position)
+    pygame.display.flip()
+
+
+# pygame and objects init
+mac_gyver = Character(maze_map)
+
+pygame.init()  # init all moduls
+window = pygame.display.set_mode((15 * TILE_SIZE, 15 * TILE_SIZE)) # création d'une fenêtre
+#fond = pygame.image.load("background.jpg").convert() # charger une image
+image_brick = pygame.image.load("brick.png").convert_alpha() # charger une image avec fond transparent
+image_floor = pygame.image.load("floor.png").convert_alpha()
+image_mac_gyver = pygame.image.load("mac_gyver.png").convert_alpha()
+image_guardian = pygame.image.load("murdoc.png").convert_alpha()
+image_brick = pygame.transform.scale2x(image_brick)
+image_floor = pygame.transform.scale2x(image_floor)
+#image.set_colorkey((255,255,255)) # rendre le blanc (valeur RGB : 255,255,255) de l'image transparent
+#fenetre.blit(brick, (320,240)) # coller une image sur la fenêtre
+
+
+display(maze_map)
+
+continuer = True
+
+# main loop
 while continuer:
     for event in pygame.event.get():   # parcours de la liste des evenements
         if event.type == QUIT:     # evenement de type QUIT
-            continuer = 0
+            continuer = False
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                mac_gyver.go("left")
+            if event.key == K_RIGHT:
+                mac_gyver.go("right")
+            if event.key == K_UP:
+                mac_gyver.go("up")
+            if event.key == K_DOWN:
+                mac_gyver.go("down")
+
+            display(maze_map)
 
 # if __name__ == "__main__":
     # do stuff
