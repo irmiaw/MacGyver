5bf2321f0742d128900d0a674ab19af89b4323fc
irmiaw <j.c.comg@gmail.com>
2017-09-16 01:24:22 +0200

    creation de la classe Lvl (et quelques experimentations)

--- game.py
+++ game.py
@@ -4,11 +4,19 @@
 # Constants
 TILE_SIZE = 40
 
-class Character():
-    def __init__(self, maze_map):
+class Lvl:
+    def __init__(self, map):
+        self.map = map
+        # self.objects
+
+    def free_path():
+        return True
+
+class Character:
+    def __init__(self, maze_map, lvl):
         self.case_position = {'x': 2, 'y': 1}
-        self.maze_map = maze_map
         self.num_items = 3  # for now Mac Gyver has all items
+        self.maze_map = maze_map
 
     def collect_items(self):
         x = self.case_position['x']
@@ -21,8 +29,11 @@ class Character():
         x = self.case_position['x']
         y = self.case_position['y']
         if direction == "left":
-            if x > 0 and self.maze_map[y][x-1] != "#":
-                self.case_position['x'] -= 1
+            if Lvl.free_path():
+                print("it works!!!")
+
+            # if x > 0 and lvl.map[y][x-1] != "#":
+            #     self.case_position['x'] -= 1
         elif direction == "right":
             if x < 14 and maze_map[y][x+1] != "#":
                 self.case_position['x'] += 1
@@ -43,7 +54,6 @@ class Character():
             return True
         else:
             return False
-
 
     @property
     def pixel_position(self):
