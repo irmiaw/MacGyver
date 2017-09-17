c3bb0c383f7342a01f9318856f0a509917c5b590
irmiaw <j.c.comg@gmail.com>
2017-09-18 00:15:27 +0200

    Deplacement de la fonction de détection de collision dans la classe Lvl et changement de la variable case_position en x, y

--- game.py
+++ game.py
@@ -1,60 +1,49 @@
 
 
+from constants import *
 
-# Constants
-TILE_SIZE = 40
 
 class Lvl:
     def __init__(self, map):
         self.map = map
         # self.objects
 
-    def free_path():
-        return True
+    def free_path(self, x, y, direction):
+        return (direction == LEFT and x > 0 and self.map[y][x-1] != "#" or
+        direction == RIGHT and x < 14 and self.map[y][x+1] != "#" or
+        direction == UP and y > 0 and self.map[y-1][x] != "#" or
+        direction == DOWN and y < 14 and self.map[y+1][x] != "#")
 
-class Character:
-    def __init__(self, maze_map, lvl):
-        self.case_position = {'x': 2, 'y': 1}
+
+class Character(Lvl):
+    def __init__(self, lvl):
+        self.x = 2 # todo: load mac position from file
+        self.y = 1
         self.num_items = 3  # for now Mac Gyver has all items
-        self.maze_map = maze_map
+        self.lvl = lvl
 
     def collect_items(self):
-        x = self.case_position['x']
-        y = self.case_position['y']
-        if self.maze_map[y][x] == '$':  # replace with a item list instead of '$'
-            self.maze_map[y][x] = ' '
+        if self.lvl.map[self.y][self.x] == '$':  # replace with a item list instead of '$'
+            self.lvl.map[self.y][self.x] = ' '
             self.num_items += 1
 
     def go(self, direction):
-        x = self.case_position['x']
-        y = self.case_position['y']
-        if direction == "left":
-            if Lvl.free_path():
-                print("it works!!!")
-
-            # if x > 0 and lvl.map[y][x-1] != "#":
-            #     self.case_position['x'] -= 1
-        elif direction == "right":
-            if x < 14 and maze_map[y][x+1] != "#":
-                self.case_position['x'] += 1
-        elif direction == "up":
-            if y > 0 and maze_map[y-1][x] != "#":
-                self.case_position['y'] -= 1
-        elif direction == "down":
-            if y < 14 and maze_map[y+1][x] != "#":
-                self.case_position['y'] += 1
-        else:
-            print("error: unknown direction")
-        self.collect_items()
+        if self.lvl.free_path(self.x, self.y, direction):
+            if direction == LEFT:
+                    self.x -= 1
+            elif direction == RIGHT:
+                    self.x += 1
+            elif direction == UP:
+                    self.y -= 1
+            elif direction == DOWN:
+                    self.y += 1
+            else:
+                print("error: unknown direction")
+            self.collect_items()
 
     def victory(self):
-        x = self.case_position['x']
-        y = self.case_position['y']
-        if self.maze_map[y][x] == '.' and self.num_items == 3:
-            return True
-        else:
-            return False
+        return self.lvl.map[self.y][self.x] == '.' and self.num_items == 3
 
     @property
     def pixel_position(self):
-        return [self.case_position['x'] * TILE_SIZE, self.case_position['y'] * TILE_SIZE]
+        return [self.x * TILE_SIZE, self.y * TILE_SIZE]
