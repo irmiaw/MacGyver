d7ffc9b483a32604adeb6297e2cc953b8d4bf5ee
irmiaw <j.c.comg@gmail.com>
2017-09-19 00:33:44 +0200

    Chergement des images

--- load.py
+++ load.py
@@ -6,6 +6,9 @@ class Images:
         self.floor = self.load_image(filenames[1])
         self.mac_gyver = self.load_image(filenames[2])
         self.guardian = self.load_image(filenames[3])
+        self.needle = self.load_image(filenames[4])
+        self.tube = self.load_image(filenames[5])
+        self.ether = self.load_image(filenames[6])
 
     def load_image(self, filename):
         return pygame.image.load(filename).convert_alpha()
