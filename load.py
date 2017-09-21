500970ccd6bb82e6f3c505171b672860e70cc8b8
irmiaw <j.c.comg@gmail.com>
2017-09-21 03:49:12 +0200

    Chargement des images a partir des données du fichier config

--- load.py
+++ load.py
@@ -1,16 +1,17 @@
-"""Load images and map"""
+"""Load images, map and config data from json file"""
+import json
 import pygame
 
-"""Load images from given filname"""
+"""Load images from given config parameters"""
 class Images:
-    def __init__(self, filenames):
-        self.brick = self.load_image(filenames[0])
-        self.floor = self.load_image(filenames[1])
-        self.mac_gyver = self.load_image(filenames[2])
-        self.guardian = self.load_image(filenames[3])
-        self.needle = self.load_image(filenames[4])
-        self.tube = self.load_image(filenames[5])
-        self.ether = self.load_image(filenames[6])
+    def __init__(self, config):
+        self.brick = self.load_image(config["brick"])
+        self.floor = self.load_image(config["floor"])
+        self.mac_gyver = self.load_image(config["mac_gyver"])
+        self.guardian = self.load_image(config["guardian"])
+        self.items = {}
+        for item in config["items"]:
+            self.items[item] = self.load_image(config["items"][item]["img"])
 
     def load_image(self, filename):
         return pygame.image.load(filename).convert_alpha()
@@ -21,9 +22,12 @@ def map(filename):
     with open(filename, "r") as f:
         map = [line for line in f]
     return map
-
 # map structure:
 # '#': brick
 # ' ': floor
 # '.': guardian
 
+"""Load config from given filename"""
+def config(filename):
+    with open(filename) as data:
+        return json.load(data)
